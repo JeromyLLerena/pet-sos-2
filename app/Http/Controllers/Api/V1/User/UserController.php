e0e59a0dff051f7b1de32bb9a6d6087848dd5c8d
Jeromy Llerena <jeromyllerena@gmail.com>
2016-11-19 03:11:21 +0000

    Fixed uploading file when request file is null.

--- app/Http/Controllers/Api/V1/User/UserController.php
+++ app/Http/Controllers/Api/V1/User/UserController.php
@@ -54,17 +54,20 @@ class UserController extends Controller
 		];
 
 		$user = $this->user_repository->save($data);
-		$pet_path = $user->id . '_' . str_random(20) . '.png';
-		$request->file('pet_photo')->move('uploads', $pet_path);
 
 		$pet_data = [
 			'name' => $request->get('pet_name'),
 			'age' => $request->get('pet_age'),
-			'photo_path' => 'uploads/' . $pet_path,
 			'height' => $request->get('pet_height'),
 			'user_id' => $user->id,
 			'race_id' => $request->get('pet_race')
 		];
+
+		if ($request->file('pet_photo')) {
+			$pet_path = $user->id . '_' . str_random(20) . '.png';
+			$request->file('pet_photo')->move('uploads', $pet_path);
+			$pet_data ['photo_path'] = 'uploads/' . $pet_path;
+		}
 
 		$pet = $this->pet_repository->save($pet_data);
 
